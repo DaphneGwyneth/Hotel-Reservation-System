b489eea50891656b6de0668a2f25b538b997b03a
DenciaGabaon <129720745+DenciaGabaon@users.noreply.github.com>
2023-06-05 19:06:09 +0800

    Option 1: No file handling and String masking

--- loginsystem.java
+++ loginsystem.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 import java.io.File;
-import java.io.FileNotFoundException;
-
-
+import java.io.FileNotFoundException;import java.io.*;
 
 public class loginsystem {
 	public static int choice; 
 	public static Scanner sc = new Scanner(System.in);
+	
 	
     public static void main (String [] args){
     	
@@ -51,53 +50,27 @@ public class loginsystem {
 
     static void adminlogin(){
         //User b;
-
+      clearScreen();
         System.out.println("+===============================================================+");
         System.out.println("|\t            Hotel Picadili Travels      \t\t|");
         System.out.println("+=============+===============================================================+=============+");
         System.out.println("----------------------------------");
         System.out.println("       Picadili Registration");
         System.out.println("----------------------------------");
-        System.out.println("Enter Email: ");
-        String email = sc.nextLine();
-        System.out.println("Enter Password: ");
-        String password = sc.nextLine();
+        System.out.print("Enter Email: ");  String email = sc.nextLine();       
+        System.out.print("Enter Password: ");  String password = sc.nextLine();
         //System.out.print("----------------------------------");
         //System.out.print("|                                  |");
         //System.out.print("----------------------------------");
-       
-        try {
-            File Obj = new File("admin.txt");
-            Scanner Reader = new Scanner(Obj);
-            while (Reader.hasNextLine()) {
-                String data = Reader.nextLine();
-                System.out.println(data);
-                sc.nextLine();
-            }
-            Reader.close();
+
+        if (email.equalsIgnoreCase("admin") && password.equalsIgnoreCase("123123")) {
+        	System.out.println("Logged in Successfuly!");
         }
-        catch (FileNotFoundException e) {
-            System.out.println("An error has occurred.");
-            e.printStackTrace();
-            sc.nextLine();
+        else {
+        	adminlogin();
         }
-       
      
     
-     /*   if (authentication(eme, password) == 1)
-        {
-            System.out.print("      Login Successful!");
-        }
-        else if (authentication(eme, password) == 0)
-        {
-            String email = String.valueOf(eme);
-            encrypt_password(password, 0xFACA);
-            String pass = String.valueOf(password);*/
-
-            
-    
-          //   save_user();
-              System.out.print("      Registration Successful!\n");
     
         }
     
@@ -146,5 +119,8 @@ public class loginsystem {
     }
     
     
+    
+    
 
 }
+
